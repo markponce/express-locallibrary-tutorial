365413f56d988fbea5f53b00cdb1428e859b69c5
Mark Ponce <mark@clearmindai.com>
2018-06-14 19:41:17 +0800

    finish adding data, routes and controller

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+
 
 var app = express();
 
@@ -31,6 +33,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
